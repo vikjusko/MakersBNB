246e7100e012aa95be52d106e45d347515b16a31
Victoria Jusko <vikjusko@gmail.com>
2020-09-28 16:10:51 +0100

    viewing accommodation and feature test

--- app.rb
+++ app.rb
@@ -5,36 +5,16 @@ require 'pg'
 class MakersBNB < Sinatra::Base
 
 	get '/' do 
-		"Welcome to MakersBNB"
+		erb :index
 	end 
 
-
-	run if app_file == $0
-
-end 
-
-require 'sinatra/base'
-require 'pg'
-
-
-class MakersBNB < Sinatra::Base
-
-	get '/' do 
-		"Welcome to MakersBNB"
+	get '/accommodation' do
+		'Nice cottage in Deptford'
 	end 
-
-
+	
 	run if app_file == $0
 
 end 
 
 
-feature 'View accomodation' do
-  scenario 'A list accmmodation is viewed on sceen' do
-    visit '/'
-    click_button("View accommodation")
-    expect(current_path).to eq("/accommodation")
-    expect(page).to have_content("Nice cottage in Depford")
-  end
-end
 
---
+++ spec/features/viewing_accommodation_spec.rb
@@ -0,0 +1,8 @@
+feature "View accomodation" do
+  scenario "A list accmmodation is viewed on sceen" do
+    visit "/"
+    click_button("View accommodation")
+    expect(current_path).to eq("/accommodation")
+    expect(page).to have_content("Nice cottage in Deptford")
+  end
+end
