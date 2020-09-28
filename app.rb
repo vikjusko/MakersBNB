4771be81830b7a269f510b34d1e182c6f41e4dba
Richard <rich_pryce@yahoo.co.uk>
2020-09-28 15:54:58 +0100

    view_accommodation test in app.rb file..

--- app.rb
+++ app.rb
@@ -12,3 +12,29 @@ class MakersBNB < Sinatra::Base
 	run if app_file == $0
 
 end 
+
+require 'sinatra/base'
+require 'pg'
+
+
+class MakersBNB < Sinatra::Base
+
+	get '/' do 
+		"Welcome to MakersBNB"
+	end 
+
+
+	run if app_file == $0
+
+end 
+
+
+feature 'View accomodation' do
+  scenario 'A list accmmodation is viewed on sceen' do
+    visit '/'
+    click_button("View accommodation")
+    expect(current_path).to eq("/accommodation")
+    expect(page).to have_content("Nice cottage in Depford")
+  end
+end
+
