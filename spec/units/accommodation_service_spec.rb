198a91a2a05c85aa2f22ab002f7b6a2f2882e9bb
Xavier Defontaine <xdefontaine@gmail.com>
2020-09-29 10:34:14 +0100

    working on AccomService test

--- spec/units/accommodation_service_spec.rb
+++ spec/units/accommodation_service_spec.rb
@@ -2,7 +2,7 @@ describe AccommodationService do
 
   describe '#.all' do
     it 'returns a list of accommodation' do
-      # 
+      AccommodationService.create(name: "Cottage", description: "Nice cottage in Deptford", location: "London", price: 40)
       accom_list = AccommodationService.all
       expect(accom_list.first.description).to eq "Nice cottage in Deptford"
     end
