7934d034107f3a809011d5e48b9134025dd90ea4
Xavier Defontaine <xdefontaine@gmail.com>
2020-09-29 16:50:48 +0100

    Updated Capybara.app to use config

--- spec/features/viewing_accommodation_spec.rb
+++ spec/features/viewing_accommodation_spec.rb
@@ -1,9 +1,13 @@
-
-feature 'View accomodation' do
+require './spec/database_helper'
+feature 'View all accomodation' do
   scenario 'A list accommodation is viewed on sceen' do
+    load_test_accom
     visit '/'
     click_button("View accommodation")
     expect(current_path).to eq("/accommodations")
-    expect(page).to have_content("Nice cottage in Deptford")
+    expect(page).to have_content("Beach shack")
+    expect(page).to have_content("Cottage")
+    
+
   end
 end
