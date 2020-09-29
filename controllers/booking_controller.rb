0871d77bb61b097a252c2f60712c3cbd3b90665f
samcolson4 <samcolson4@gmail.com>
2020-09-29 17:02:33 +0100

    refactored

--- controllers/booking_controller.rb
+++ controllers/booking_controller.rb
@@ -7,7 +7,7 @@ class BookingController < Sinatra::Base
 
   post '/booking/:id/new' do
     accommodation_id = @accommodation_id #this will be passed in from the accommodation_controller
-    
+
     BookingService.create(accommodation_id: accommodation_id, user_email: params[:user_email], date: params[:date])
 
     erb :booking_request
