4f0983e8973b7710fae66189115ba8791adc1a3d
Veronica Lee <veronicavlee@icloud.com>
2020-09-29 17:52:40 +0100

    Add AccommodationService.find(id) method

--- spec/features/viewing_all_accom_spec.rb
+++ spec/features/viewing_all_accom_spec.rb
@@ -1,13 +1,12 @@
 require './spec/database_helper'
-feature 'View all accomodation' do
-  scenario 'A list accommodation is viewed on sceen' do
+
+feature 'View all accommodation' do
+  scenario 'A list of accommodation is viewed on sceen' do
     load_test_accom
     visit '/'
     click_button("View accommodation")
     expect(current_path).to eq("/accommodations")
     expect(page).to have_content("Beach shack")
     expect(page).to have_content("Cottage")
-    
-
   end
 end
