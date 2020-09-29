7f5c652346a64a62fe571d1aa7558430024e5788
samcolson4 <samcolson4@gmail.com>
2020-09-29 16:02:57 +0100

    Added test for .all

--- spec/units/booking_service_spec.rb
+++ spec/units/booking_service_spec.rb
@@ -7,20 +7,21 @@ describe BookingService do
 		accommodation = AccommodationService.create(name: "Nice cottage", description: "Cottage in London", location: "London", price: 40)
 		bookingservice = BookingService.create(accommodation_id: accommodation.id, user_email: "test@test.com", date: "2020-09-29")
     expect(bookingservice).to be_a Booking
-    #expect(bookingservice.id).to eq persisted_data["id"]
     expect(bookingservice.accommodation_id).to eq accommodation.id
 		expect(bookingservice.user_email).to eq("test@test.com")
 		expect(bookingservice.date).to eq("2020-09-29")
   	end
   end
 
-  # describe '#all' do
-  #   it "produces a list of all bookings" do
-  #     bookingservice = BookingService.create(user_email: "test@test.com", date: Today.new)
-  #     bookings = BookingService.all
-	# 		#expect(bookings.first.accommodation_id).to eq '4'
-	# 		expect(bookings).to be_a BookingService
-  #     expect(bookings.first.user_email).to eq "test@test.com"
-  #   end
-  # end
+  describe '#all' do
+    it "produces a list of all bookings" do
+      accommodation = AccommodationService.create(name: "Nice cottage", description: "Cottage in London", location: "London", price: 40)
+
+      bookingservice = BookingService.create(accommodation_id: accommodation.id, user_email: "test@test.com", date: "2020-09-29")
+      
+      bookings = BookingService.all
+			expect(bookings).to be_a Array
+      expect(bookings.first.user_email).to eq "test@test.com"
+    end
+  end
 end 
