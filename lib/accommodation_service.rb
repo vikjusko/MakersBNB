7efa65e35e4ed69e984453dbde7fae4f2b4cef55
Richard <rich_pryce@yahoo.co.uk>
2020-09-30 13:06:06 +0100

    host_id added to Accommodation AccommodationService

--- lib/accommodation_service.rb
+++ lib/accommodation_service.rb
@@ -5,16 +5,16 @@ class AccommodationService
 
   def self.all
     result = DatabaseConnection.query("SELECT * FROM accommodation;")
-    result.map {|accommodation| Accommodation.new(id: accommodation['id'], name: accommodation['name'], description: accommodation['description'], location: accommodation['location'], price: accommodation['price']) }
+    result.map {|accommodation| Accommodation.new(id: accommodation['id'], name: accommodation['name'], description: accommodation['description'], location: accommodation['location'], price: accommodation['price'], host_id: accommodation['host_id']) }
   end
 
   def self.create(name:, description:, location:, price:)
-    result = DatabaseConnection.query("INSERT INTO accommodation (name, description, location, price) VALUES('#{name}', '#{description}', '#{location}', '#{price}') RETURNING id, name, description, location, price;")
-    Accommodation.new(id: result[0]["id"], name: result[0]["name"], description: result[0]['description'], location: result[0]["location"], price: result[0]['price'])
+    result = DatabaseConnection.query("INSERT INTO accommodation (name, description, location, price, host_id) VALUES('#{name}', '#{description}', '#{location}', '#{price}', '#{host_id}) RETURNING id, name, description, location, price, host_id;")
+    Accommodation.new(id: result[0]["id"], name: result[0]["name"], description: result[0]['description'], location: result[0]["location"], price: result[0]['price'], host_id: result[0]['host_id'])
   end
 
   def self.find(id)
     result = DatabaseConnection.query("SELECT * FROM accommodation WHERE id=#{id};")[0]
-    Accommodation.new(id: result['id'], name: result['name'], description: result['description'], location: result['location'], price: result['price'])
+    Accommodation.new(id: result['id'], name: result['name'], description: result['description'], location: result['location'], price: result['price'], host_id: result['host_id'])
   end
 end 
