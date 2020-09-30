a83285aebc59b95371338fb28c90a57190898e5e
Veronica Lee <veronicavlee@icloud.com>
2020-09-30 12:13:49 +0100

    Add migration scripts and truncate all tables in helper

--- spec/database_helper.rb
+++ spec/database_helper.rb
@@ -1,7 +1,7 @@
 
 def setup_and_clear_test_database
   connection = PG.connect("host=makersbnb.c4gsfvuzdyl3.eu-west-2.rds.amazonaws.com port=5433 user=postgres password=785njsjas88## dbname=makersbnb_test")
-  connection.exec('TRUNCATE TABLE accommodation, booking;')
+  connection.exec('TRUNCATE TABLE accommodation, booking, users, availability;')
 end
 
 def load_test_accom
