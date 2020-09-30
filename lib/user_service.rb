3039a174a5bc068d20e9b8234a05b540ee21c72b
Victoria Jusko <vikjusko@gmail.com>
2020-09-30 16:31:01 +0100

    Added register, login, logout, get_user, current_user and checking for password

--- lib/user_service.rb
+++ lib/user_service.rb
@@ -1,41 +1,40 @@
 require 'bcrypt'
 
-require './lib/user'
-require './lib/database_connection'
+require_relative './user'
+require_relative "./database_connection"
 
 
 class UserService
 
-  def self.register(email:, name:, password:)
-    result = DatabaseConnection.query("INSERT INTO users (email, name, password)
-        VALUES('#{email}', '#{name}', '#{BCrypt::Password.create(password)}') RETURNING id, email, name;")
-    @user = User.new(id: result[0]['id'], email: result[0]['email'], name: result[0]['name'])
+  def self.register(name:, email:,  password:)
+    result = DatabaseConnection.query("INSERT INTO users (name, email, password) VALUES('#{name}', '#{email}', '#{BCrypt::Password.create(password)}') RETURNING id, name, email;")
+    @user = User.new(id: result[0]['id'], name: result[0]['name'], email: result[0]['email'])
   end
-
-  # def self.login(username, password)
-  #   user = get_user(username)
-  #   return false if user == false
-  #   return false unless password_match?(user, password)
-  #   @user = User.new(user['username'], user['name'], user['email'])
-  # end
-  #
-  # def self.logout
-  #   @user = nil
-  # end
-  #
-  # def self.current_user
-  #   @user
-  # end
+  
+  def self.login(email:, password:)
+    user = get_user(email)
+    return false if user == false
+    return false unless password_match?(user, password)
+    @user = User.new(id: user['id'], name: user['name'], email: user['email'])
+  end
+  
+  def self.logout
+    @user = nil
+  end
+  
+  def self.current_user
+    @user
+  end
 
   private
 
-  # def self.get_user(username)
-  #   result = DatabaseConnection.query("SELECT * FROM users WHERE username='#{username}'")
-  #   false unless result.count == 1
-  #   result[0]
-  # end
-  #
-  # def self.password_match?(user, password)
-  #   BCrypt::Password.new(user['password']) == password
-  # end
+  def self.get_user(email)
+    result = DatabaseConnection.query("SELECT * FROM users WHERE email = '#{email}';")
+    return false unless result.count == 1
+    result[0]
+  end
+  
+  def self.password_match?(user, password)
+    BCrypt::Password.new(user['password']) == password
+  end
 end
