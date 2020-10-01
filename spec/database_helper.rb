22e2183c300ee1a094e1e1630e19faaab7d0ad2e
samcolson4 <samcolson4@gmail.com>
2020-10-01 14:50:13 +0100

    Adding host feature tests

--- spec/database_helper.rb
+++ spec/database_helper.rb
@@ -10,14 +10,12 @@ def load_test_accom
   connection.exec("INSERT INTO accommodation(id, name, description, location, price) VALUES(2, 'Beach shack', 'A first floor apartment with spectacular views and bright and airy decor that echoes the sea in every room.', 'Cornwall', 60);")
 end
 
-<<<<<<< HEAD
 def persisted_data(id:)
   connection = PG.connect("host=makersbnb.c4gsfvuzdyl3.eu-west-2.rds.amazonaws.com port=5433 user=postgres password=785njsjas88## dbname=makersbnb_test")
   result = connection.query("SELECT * FROM accommodation WHERE id = #{id};")
   result.first
-=======
+
 def load_test_user
   connection = PG.connect("host=makersbnb.c4gsfvuzdyl3.eu-west-2.rds.amazonaws.com port=5433 user=postgres password=785njsjas88## dbname=makersbnb_test")
   connection.exec("INSERT INTO users (name, email, password) VALUES('test_name', 'test_email@email.com', '#{BCrypt::Password.create('password123')}') RETURNING id, name, email;")
->>>>>>> Adding host feature tests
 end
