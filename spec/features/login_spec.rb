c4b00d3c5c4ecf8ceb5d4a3d9f4d9de11dc3a705
Victoria Jusko <vikjusko@gmail.com>
2020-10-01 12:01:29 +0100

    added log in and log out feature tests

---
+++ spec/features/login_spec.rb
@@ -0,0 +1,25 @@
+feature 'User logging in' do 
+	scenario "user can log into the website if the user exists" do 
+		UserService.register(name: 'test', email: 'test@test.com', password: '1234567')
+		visit '/'
+		click_button('Log in')
+		expect(page).to have_current_path('/login')
+		fill_in('email', with: 'test@test.com')
+		fill_in('passsword', with: '1234567')
+		click_button('Log in')
+		expect(page).to have_current_path('/')
+		expect(page).to have_content("Welcome test")
+		log_out
+	end 
+	
+	scenario 'user cannot login if they havent registered' do
+		visit '/'
+		click_button('Log in')
+		expect(page).to have_current_path('/login')
+		fill_in('email', with: 'test@test.co.uk')
+		fill_in('passsword', with: '12345')
+		click_button('Log in')
+		expect(page).to have_current_path('/login')
+		expect(page).to have_content("This user does not exist")
+	end 
+end 
--- spec/features/register_user_spec.rb
+++ spec/features/register_user_spec.rb
@@ -1,12 +1,18 @@
 feature 'Register user' do
   scenario 'User registration with unique email is successful' do
-    visit '/sign-up'
+    visit '/'
+    click_button 'Sign up'
+    expect(page).to have_current_path('/sign-up')
     fill_in 'email', with: '123@email.com'
     fill_in 'name', with: 'Test User'
     fill_in 'password', with: 'h3ll0wor!d'
     click_button 'Sign up'
+    expect(page).to have_current_path('/')
+    expect(page).not_to have_button('Sign up')
     expect(page).to have_content('Welcome Test User')
+    log_out
   end
+
 end
 
 # TODO: test that you are redirected back to previous page
