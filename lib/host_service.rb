02638c72fe3f0e75a2adc5dcdb90467e3fb7a02f
Veronica Lee <veronicavlee@icloud.com>
2020-10-01 16:56:06 +0100

    Fix BookingService tests to use host_id

--- lib/host_service.rb
+++ lib/host_service.rb
@@ -8,10 +8,10 @@ class HostService
   # end
 
   def self.find_pending_requests(host_id)
-    result = DatabaseConnection.query("SELECT b.id, b.accommodation_id, b.user_email, b.date FROM booking as b JOIN accommodation as a
+    result = DatabaseConnection.query("SELECT b.id, b.accommodation_id, b.user_email, b.date, b.status FROM booking as b JOIN accommodation as a
       ON b.accommodation_id = a.id
       WHERE host_id = '#{host_id}';")
-    # TODO WHERE b.status = 'PENDING' once we set the booking status
-    result.map { |b| Booking.new(id: b['id'], accommodation_id: b['accommodation_id'], user_email: b['user_email'], date: b['date']) }
+      AND b.status = 'PENDING' once we set the booking status
+    result.map { |b| Booking.new(id: b['id'], accommodation_id: b['accommodation_id'], user_email: b['user_email'], date: b['date'], status: b['status']) }
   end
 end
--- spec/units/booking_service_spec.rb
+++ spec/units/booking_service_spec.rb
@@ -1,11 +1,11 @@
 require 'booking_service'
 require 'accommodation'
 
-describe BookingService do 
-	describe "#create" do
+describe BookingService do
+	describe ".create" do
 		it "creates a new booking with the details given and sets status to PENDING" do
-      user = UserService.create
-      accommodation = AccommodationService.create(name: "Nice cottage", description: "Cottage in London", location: "London", price: 40)
+      host = DatabaseConnection.query("INSERT INTO users(name, email, password) VALUES('Test User', 'test@test.com', 'asdfadf') RETURNING id;")[0]
+      accommodation = AccommodationService.create(name: "Nice cottage", description: "Cottage in London", location: "London", price: 40, host_id: host['id'])
       booking = BookingService.create(accommodation_id: accommodation.id, user_email: "test@test.com", date: "2020-09-29")
       expect(booking).to be_a Booking
       expect(booking.accommodation_id).to eq accommodation.id
@@ -15,15 +15,15 @@ describe BookingService do
   	end
   end
 
-  describe '#all' do
-    it "produces a list of all bookings" do
-      accommodation = AccommodationService.create(name: "Nice cottage", description: "Cottage in London", location: "London", price: 40)
-
-      bookingservice = BookingService.create(accommodation_id: accommodation.id, user_email: "test@test.com", date: "2020-09-29")
+  describe '.all' do
+    it "returns a list of all bookings" do
+      host = DatabaseConnection.query("INSERT INTO users(name, email, password) VALUES('Test User', 'test@test.com', 'asdfadf') RETURNING id;")[0]
+      accommodation = AccommodationService.create(name: "Nice cottage", description: "Cottage in London", location: "London", price: 40, host_id: host['id'])
+      booking = BookingService.create(accommodation_id: accommodation.id, user_email: "test@test.com", date: "2020-09-29")
 
       bookings = BookingService.all
 			expect(bookings).to be_a Array
-      expect(bookings.first.user_email).to eq "test@test.com"
+      expect(bookings.first.user_email).to eq booking.user_email
     end
   end
 end 
