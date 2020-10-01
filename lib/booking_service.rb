a0e86f590fc9ca0162c59320ebe6344d371a3126
Richard <rich_pryce@yahoo.co.uk>
2020-10-01 18:04:28 +0100

    updated PENDING status

--- lib/booking_service.rb
+++ lib/booking_service.rb
@@ -1,23 +1,21 @@
 require_relative 'booking'
 require_relative 'database_connection'
-#require_relative '../database_connection_setup.rb'
 
 class BookingService
-
-	def self.create(accommodation_id:, user_email:, date: )
-    result = DatabaseConnection.query("INSERT INTO booking(accommodation_id, user_email, date) VALUES(#{accommodation_id}, '#{user_email}', '#{date}') RETURNING id, accommodation_id, user_email, date;")
-		Booking.new(id: result[0]["id"], accommodation_id: result[0]['accommodation_id'], user_email: result[0]["user_email"], date: result[0]["date"])
+  DEFAULT_STATUS = 'PENDING'
+ def self.create(accommodation_id:, user_email:, date: )
+    result = DatabaseConnection.query("INSERT INTO booking(accommodation_id, user_email, date, status) VALUES(#{accommodation_id}, '#{user_email}', '#{date}', '#{DEFAULT_STATUS}') RETURNING id, accommodation_id, user_email, date, status;")
+  Booking.new(id: result[0]['id'], accommodation_id: result[0]['accommodation_id'], user_email: result[0]['user_email'], date: result[0]['date'], status: result[0]['status'])
   end
-
   def self.all
     result = DatabaseConnection.query("SELECT * FROM booking")
-    
     result.map { |booking| 
       Booking.new(
         id: booking['id'],
         accommodation_id: booking['accommodation_id'],
         user_email: booking['user_email'],
-        date: booking['date']
+        date: booking['date'],
+        status: booking['status']
         )
     }
   end
--- lib/host_service.rb
+++ lib/host_service.rb
@@ -8,11 +8,11 @@ class HostService
   # end
 
   def self.find_pending_requests(host_id)
-    result = DatabaseConnection.query("SELECT b.id, b.accommodation_id, b.user_email, b.date FROM booking as b JOIN accommodation as a
+    result = DatabaseConnection.query("SELECT b.id, b.accommodation_id, b.user_email, b.date, b.status FROM booking as b JOIN accommodation as a
       ON b.accommodation_id = a.id
       WHERE host_id = '#{host_id}'
       AND b.status = 'PENDING';")
-    result.map { |b| Booking.new(id: b['id'], accommodation_id: b['accommodation_id'], user_email: b['user_email'], date: b['date']) }
+    result.map { |b| Booking.new(id: b['id'], accommodation_id: b['accommodation_id'], user_email: b['user_email'], date: b['date'], status: b['status']) }
   end
 
 
